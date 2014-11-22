9bbc483087f0b998c2eb1ad233851b7fda27eb23
tony-minihum <toshiharuhot@yahoo.co.jp>
2014-11-22 21:23:55 +0900

    draw TRIANGLE_FAN

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -1,7 +1,10 @@
 #define GLFW_DLL
 
+#define _USE_MATH_DEFINES
+
 #include <GL/glfw.h>
 #include <cstdlib>
+#include <cmath>
 
 #if defined (_MSC_VER)
 #pragma comment(lib, "GLFWDLL.lib")
@@ -34,22 +37,24 @@ int main()
 	while (glfwGetWindowParam(GLFW_OPENED))
 	{
 		// Set clear color, and then, clear buffer.
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Set vertex array.
-		static const GLfloat vtx[] = {
-			0.0f, 0.433f,
-			-0.5f, -0.433f,
-			0.5f, -0.433f,
-		};
+		GLfloat vtx[20 * 2];
+
+		for (int i = 0; i < 20; ++i) {
+			GLfloat angle = static_cast<GLfloat>((M_PI * 2.0 * i) / 20);
+			vtx[i * 2] = std::sin(angle) * 0.5f;
+			vtx[i * 2 + 1] = std::cos(angle) * 0.6f;
+		}
 		glVertexPointer(2, GL_FLOAT, 0, vtx);	// glVertexPinter(size, type, stride, pointer);
 												// size := Specifies the number of coordinates per vertex. Must be 2, 3, or 4. The initial value is 4.
-		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 
 		// Set mode
 		glEnableClientState(GL_VERTEX_ARRAY);
-		glDrawArrays(GL_TRIANGLES, 0, 3);	// glDrawArrays(mode, first, count);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 20);	// glDrawArrays(mode, first, count);
 
 		// Reset mode
 		glDisableClientState(GL_VERTEX_ARRAY);
