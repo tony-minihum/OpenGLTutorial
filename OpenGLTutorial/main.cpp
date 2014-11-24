7705443daad0e106445c2c57ac7cc7b5b6d91ca1
tony-minihum <toshiharuhot@yahoo.co.jp>
2014-11-24 14:38:47 +0900

    Viewport, Projection matrix

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -67,7 +67,7 @@ int main()
 	}
 
 	// Initialize OpenGL and create window
-	if (!glfwOpenWindow(640, 480, // width, height
+	if (!glfwOpenWindow(500, 400, // width, height
 		0, 0, 0,              // number of R bit, G, B  // when set 0, default value is used.
 		0,                    // number of A bit
 		0, 0,                 // number of depth bit, stencil
@@ -96,70 +96,39 @@ int main()
 	// 
 	glfwSwapInterval(1);
 
-	float pos_x = 0.0f;
-	float pos_y = 0.0f;
+	// Setup Viewport.
+	// (-1, -1) - (1, 1) -> (0, 0) - (500, 400)
+	glViewport(0, 0, 500, 400);
+
+	// Setup Projection matrix. ìäâeçsóÒ
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();  // Load Identity matrix.
+	glOrtho(0.0f, 500.0f, 0.0f, 400.0f, -1.0f, 1.0f); // glOrtho(left, right, bottom, top, near, far);
 
 	// Loop while window opened.
 	while (glfwGetWindowParam(GLFW_OPENED)) {
-		if (glfwGetKey(GLFW_KEY_ESC) == GLFW_PRESS)
-			break;
-
-		if (glfwGetKey( 'A' ) == GLFW_PRESS)
-			pos_x -= 0.02f;
-		if (glfwGetKey( 'D') == GLFW_PRESS)
-			pos_x += 0.02f;
-		if (glfwGetKey( 'W' ) == GLFW_PRESS)
-			pos_y += 0.02f;
-		if (glfwGetKey( 'X' ) == GLFW_PRESS)
-			pos_y -= 0.02f;
-
-		if (glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
-			int mouse_x, mouse_y;
-			glfwGetMousePos(&mouse_x, &mouse_y);
-			pos_x = mouse_x * 2.0f / 640.0f - 1.0f;
-			pos_y = -(mouse_y * 2.0f / 480.0f - 1.0f);
-		}
-
 		// Set clear color, and then, clear buffer.
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Set vertex array.
 		const GLfloat vtx[] = {  // modify static const to const 
-			-0.8f + pos_x, -0.8f + pos_y,
-			0.8f + pos_x, -0.8f + pos_y,
-			0.8f + pos_x, 0.8f + pos_y,
-			-0.8f+ pos_x, 0.8f + pos_y,
+			100.0f, 100.0f,
+			200.0f, 100.0f,
+			200.0f, 200.0f,
+			100.0f, 200.0f
 		};
 		glVertexPointer(2, GL_FLOAT, 0, vtx);	// glVertexPinter(size, type, stride, pointer);
 											// size := Specifies the number of coordinates per vertex. Must be 2, 3, or 4. The initial value is 4.
 		
-		static const GLfloat texture_uv[] = {
-			0.0f, 1.0f,
-			1.0f, 1.0f,
-			1.0f, 0.0f,
-			0.0f, 0.0f
-		};
-		glTexCoordPointer(2, GL_FLOAT, 0, texture_uv);
-
-		// Enable blending
-		glEnable(GL_BLEND);
-
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-		glEnable(GL_TEXTURE_2D);
+		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
 		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
 		glDrawArrays(GL_QUADS, 0, 4);	// glDrawArrays(mode, first, count);
 
 		// Reset mode
 		glDisableClientState(GL_VERTEX_ARRAY);
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-
-		glDisable(GL_TEXTURE_2D);
-		glDisable(GL_BLEND);
 
 		glfwSwapBuffers();
 	}
