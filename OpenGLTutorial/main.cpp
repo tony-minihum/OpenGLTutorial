450ff04bb762b1e954048bad77350a61f7fc0e51
tony-minihum <toshiharuhot@yahoo.co.jp>
2014-12-01 00:29:56 +0900

    kaleidoscope

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -1,6 +1,8 @@
 #define GLFW_DLL
 
 #define GLEW_STATIC
+#define _USE_MATH_DEFINES
+
 #if defined (_MSC_VER)
 #include <GL/glew.h>
 #endif
@@ -8,6 +10,7 @@
 #include <GL/glfw.h>
 #include <cstdlib>
 
+#include <cmath>
 #include <fstream>
 #include <vector>
 
@@ -67,7 +70,7 @@ int main()
 	}
 
 	// Initialize OpenGL and create window
-	if (!glfwOpenWindow(640, 480, // width, height
+	if (!glfwOpenWindow(500, 500, // width, height
 		0, 0, 0,              // number of R bit, G, B  // when set 0, default value is used.
 		0,                    // number of A bit
 		0, 0,                 // number of depth bit, stencil
@@ -77,14 +80,15 @@ int main()
 		return EXIT_FAILURE;
 	}
 
+	/*
 	GLuint texture_id;
 	glGenTextures(1, &texture_id);
-	
 	if (!setupTexture(texture_id, "sample.raw",256, 256)) {
 		glDeleteTextures(1, &texture_id);
 		glfwTerminate();
 		return EXIT_FAILURE;
 	}
+	*/
 
 #if defined (_MSC_VER)
 	if (glewInit() != GLEW_OK) {
@@ -135,78 +139,94 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		angle += 0.02f;
-		const float x_ofs = std::sin(angle) * 0.5f;
+		static const float triangle[] = {
+			0.0f, 1.0f,
+			-0.8658f, -0.49956f,
+			0.8658f, 0.49956f
+		};
 
 		// Set vertex array.
-		const GLfloat vtx[] = {
-			-0.5f + x_ofs, -0.5f,
-			0.5f + x_ofs, -0.5f,
-			0.5f + x_ofs, 0.5f,
-			-0.5f + x_ofs, 0.5f,
-		};
+		GLfloat vtx[3 * 2];
+
+		for (int i = 0; i < 3; ++i) {
+			const float x = triangle[i * 2];
+			const float y = triangle[i * 2 + 1];
+			float sin_r = std::sin(angle);
+			float cos_r = std::cos(angle);
+			vtx[i * 2]     = x * cos_r - y * sin_r;
+			vtx[i * 2 + 1] = x * sin_r + y * cos_r;
+		}
+
 		glVertexPointer(2, GL_FLOAT, 0, vtx);	// glVertexPinter(size, type, stride, pointer);
 											// size := Specifies the number of coordinates per vertex. Must be 2, 3, or 4. The initial value is 4.
 		
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		static const GLfloat color[] = {
+			1.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 1.0f
+		};
+		glColorPointer(3, GL_FLOAT, 0, color);
 
 		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_COLOR_ARRAY);
 
-		glDrawArrays(GL_QUADS, 0, 4);	// glDrawArrays(mode, first, count);
+		glDrawArrays(GL_TRIANGLES, 0, 3);	// glDrawArrays(mode, first, count);
 
 		// Reset mode
+		glDisableClientState(GL_COLOR_ARRAY);
 		glDisableClientState(GL_VERTEX_ARRAY);
 
 		///
 		glBindFramebuffer(GL_FRAMEBUFFER, current_framebuffer);
 
 		// Setup Viewport.
-		glViewport(0, 0, 640, 480);
+		glViewport(0, 0, 500, 500);
 
 		// Set clear color, and then, clear buffer.
 		glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		for (int y = 0; y < 3; ++y) {
-			for (int x = 0; x < 3; ++x) {
-				float x_ofs = x * 0.55f - 0.8f;
-				float y_ofs = y * 0.55f - 0.8f;
-				const GLfloat vtx2[] = {
-					0.0f + x_ofs, 0.0f + y_ofs,
-					0.5f + x_ofs, 0.0f + y_ofs,
-					0.5f + x_ofs, 0.5f + y_ofs,
-					0.0f + x_ofs, 0.5f + y_ofs,
-				};
-				glVertexPointer(2, GL_FLOAT, 0, vtx2);
-				glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		GLfloat vtx2[20 * 2];
+		
+		vtx2[0] = 0.0f;
+		vtx2[1] = 0.0f;
+		
+		for (int i = 0; i < 19; ++i) {
+			GLfloat angle = static_cast<GLfloat>((M_PI * 2.0 * i) / 18);
+			vtx2[(i + 1) * 2] = std::sin(angle);
+			vtx2[(i + 1) * 2 + 1] = std::cos(angle);
+		}
+		glVertexPointer(2, GL_FLOAT, 0, vtx2);
+		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
-				static const GLfloat texture_uv[] = {
-					0.0f, 0.0f,
-					1.0f, 0.0f,
-					1.0f, 1.0f,
-					0.0f, 1.0f,
-				};
-				glTexCoordPointer(2, GL_FLOAT, 0, texture_uv);
+		GLfloat texture_uv[20 * 2];
+		texture_uv[0] = 0.1f;
+		texture_uv[1] = 0.1f;
 
-				glEnable(GL_TEXTURE_2D);
-				glBindTexture(GL_TEXTURE_2D, fbo_texture_id);
-
-				glEnableClientState(GL_VERTEX_ARRAY);
-				glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-				glDrawArrays(GL_QUADS, 0, 4);	// glDrawArrays(mode, first, count);
-
-				// Reset mode
-				glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-				glDisableClientState(GL_VERTEX_ARRAY);
-
-				glDisable(GL_TEXTURE_2D);
-			}
+		for (int i = 0; i < 20; ++i) {
+			texture_uv[i * 2] = (i & 1) ? 0.9f : 0.1f;
+			texture_uv[i * 2 + 1] = 0.9f;
 		}
 
+		glTexCoordPointer(2, GL_FLOAT, 0, texture_uv);
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, fbo_texture_id);
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 20);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisable(GL_TEXTURE_2D);
+		
 		glfwSwapBuffers();
 	}
 	
-	glDeleteTextures(1, &texture_id);
+	//glDeleteTextures(1, &texture_id);
+
 	glDeleteTextures(1, &fbo_texture_id);
 	glDeleteFramebuffers(1, &framebuffer_id);
 
